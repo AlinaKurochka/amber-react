ac0d2a9e2f7b5eb3763e6074e4d82949e9df78f4
AlinaKurochka <alinayaremachenko@gmail.com>
2017-12-27 17:31:54 +0200

    Changed Review Section & add Review Component

--- src/sections/ReviewSection.js
+++ src/sections/ReviewSection.js
@@ -1,69 +1,65 @@
 import React, { Component } from 'react';
+import ReviewItem from '../components/ReviewItem';
 
 class ReviewSection extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      id: "carouselReview",
+      items: [
+        {
+          "content": "Thereby, the whale commanders are enabled to recognise each other upon the ocean, even at considerable distances and with no small facility.",
+          "name": "Herman Melville",
+          "company": "Moby-Dick"
+        },
+        {
+          "content": "Thereby, the whale commanders are enabled to recognise each other upon the ocean, even at considerable distances and with no small facility.",
+          "name": "Herman Melville",
+          "company": "Moby-Dick"
+        },
+        {
+          "content": "Thereby, the whale commanders are enabled to recognise each other upon the ocean, even at considerable distances and with no small facility.",
+          "name": "Herman Melville",
+          "company": "Moby-Dick"
+        },
+        {
+          "content": "Thereby, the whale commanders are enabled to recognise each other upon the ocean, even at considerable distances and with no small facility.",
+          "name": "Herman Melville",
+          "company": "Moby-Dick"
+        },
+        {
+          "content": "Thereby, the whale commanders are enabled to recognise each other upon the ocean, even at considerable distances and with no small facility.",
+          "name": "Herman Melville",
+          "company": "Moby-Dick"
+        },
+        {
+          "content": "Thereby, the whale commanders are enabled to recognise each other upon the ocean, even at considerable distances and with no small facility.",
+          "name": "Herman Melville",
+          "company": "Moby-Dick"
+        }
+      ]
+    }
+  }
   render() {
     return (
       <section className="review-section">
         <div className="container">
-          <div id="carouselReview" className="carousel slide carousel-center" data-ride="carousel">
+          <div id={this.state.id} className="carousel slide carousel-center" data-ride="carousel">
             <div className="carousel-inner" role="listbox">
-              <div className="carousel-item active">
-                <blockquote className="review-item">
-                  <div className="review-item-content">
-                    <p>Thereby, the whale commanders are enabled to recognise each other upon the ocean, even at considerable distances and with no small facility.</p>
-                  </div>
-                  <div className="review-item-footer">
-                    <p>— Moby-Dick, Herman Melville</p>
-                  </div>
-                </blockquote>
-              </div>
-              <div className="carousel-item">
-                <blockquote className="review-item">
-                  <div className="review-item-content">
-                    <p>Thereby, the whale commanders are enabled to recognise each other upon the ocean, even at considerable distances and with no small facility.</p>
-                  </div>
-                  <div className="review-item-footer">
-                    <p>— Moby-Dick, Herman Melville</p>
-                  </div>
-                </blockquote>
-              </div>
-              <div className="carousel-item">
-                <blockquote className="review-item">
-                  <div className="review-item-content">
-                    <p>Thereby, the whale commanders are enabled to recognise each other upon the ocean, even at considerable distances and with no small facility.</p>
-                  </div>
-                  <div className="review-item-footer">
-                    <p>— Moby-Dick, Herman Melville</p>
-                  </div>
-                </blockquote>
-              </div>
-              <div className="carousel-item">
-                <blockquote className="review-item">
-                  <div className="review-item-content">
-                    <p>Thereby, the whale commanders are enabled to recognise each other upon the ocean, even at considerable distances and with no small facility.</p>
-                  </div>
-                  <div className="review-item-footer">
-                    <p>— Moby-Dick, Herman Melville</p>
-                  </div>
-                </blockquote>
-              </div>
-              <div className="carousel-item">
-                <blockquote className="review-item">
-                  <div className="review-item-content">
-                    <p>Thereby, the whale commanders are enabled to recognise each other upon the ocean, even at considerable distances and with no small facility.</p>
-                  </div>
-                  <div className="review-item-footer">
-                    <p>— Moby-Dick, Herman Melville</p>
-                  </div>
-                </blockquote>
-              </div>
+              {
+                this.state.items.map( (review, index) => {
+                  let status = (index == 0) ? 'active' : '';
+                  return <ReviewItem key={`ReviewItem-${index}`} status={status} content={review.content} name={review.name} company={review.company} />
+                })
+              }
             </div>
             <ol className="carousel-indicators review-carousel-indicators">
-              <li data-target="#carouselReview" data-slide-to="0" className="active"></li>
-              <li data-target="#carouselReview" data-slide-to="1"></li>
-              <li data-target="#carouselReview" data-slide-to="2"></li>
-              <li data-target="#carouselReview" data-slide-to="3"></li>
-              <li data-target="#carouselReview" data-slide-to="4"></li>
+              {
+                this.state.items.map( (review, index) => {
+                  let status = (index == 0) ? 'active' : '';
+                  return <li key={`ReviewIndicator-${index}`} data-target={`#${this.state.id}`} data-slide-to={index} className={status}></li>;
+                } )
+              }
             </ol>
           </div>
         </div>
